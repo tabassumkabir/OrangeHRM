ab6f529fe0821fd78af8c60796782162b8bc10c8
Tabassum Kabir <tabassumkabir123@gmail.com>
2023-08-08 00:37:48 +0600

    update

--- cypress/e2e/Modules/Admin.cy.js
+++ cypress/e2e/Modules/Admin.cy.js
@@ -22,7 +22,7 @@ describe('Admin',()=>{
         //select ess
         cy.contains('ESS').click()
         //type employee name
-        cy.get("input[placeholder='Type for hints...']").type("Admin  07")
+        cy.get("input[placeholder='Type for hints...']").type("Admin")
         //status 
         cy.get('body > div:nth-child(3) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > form:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(4) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1)').click()
         //select status 
@@ -31,5 +31,26 @@ describe('Admin',()=>{
         cy.get('.orangehrm-header-container > .oxd-button').click()
 
         cy.wait(3000)
+
+        //user role click 
+        cy.get("body > div:nth-child(3) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > form:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1)").click()
+        //select role
+        cy.contains("ESS").click()
+        //employee name
+        cy.get("input[placeholder='Type for hints...']").type("Admin")
+        //status 
+        cy.get("body > div:nth-child(3) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > form:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1)").click()
+        //select status
+        cy.contains("Enabled")
+        //username
+        cy.xpath("(//input[@class='oxd-input oxd-input--active'])[2]").type("new")
+        //pass
+        cy.xpath("(//input[@type='password'])[1]").type("admin123")
+        //confirm pass
+        cy.get("//input[@type='password'])[2]").type("admin123")
+        //click on save
+        cy.get("button[type='submit']").click()
+
+
     })
 })
