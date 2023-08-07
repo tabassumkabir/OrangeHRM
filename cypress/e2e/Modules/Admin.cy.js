87d8cd9118702f4f44e4cea85f41edfe41ed7c7d
Tabassum Kabir <tabassumkabir123@gmail.com>
2023-08-07 21:56:47 +0600

    admin

--- cypress/e2e/Modules/Admin.cy.js
+++ cypress/e2e/Modules/Admin.cy.js
@@ -1,3 +1,5 @@
+require('cypress-xpath')
+
 describe('Admin',()=>{
 
     beforeEach('login',()=>{
@@ -11,6 +13,23 @@ describe('Admin',()=>{
         cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
 
         //click on admin 
-        cy.get('.oxd-main-menu-item.active').click()
+        cy.get(':nth-child(1) > .oxd-main-menu-item').click()
+
+        //username
+        cy.xpath("(//input[@class='oxd-input oxd-input--active'])[2]").type("new")
+        //user role
+        cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-select-wrapper > .oxd-select-text').click()
+        //select ess
+        cy.contains('ESS').click()
+        //type employee name
+        cy.get("input[placeholder='Type for hints...']").type("Admin  07")
+        //status 
+        cy.get('body > div:nth-child(3) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > form:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(4) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1)').click()
+        //select status 
+        cy.contains('Enabled').click()
+        //click on add button 
+        cy.get('.orangehrm-header-container > .oxd-button').click()
+
+        cy.wait(3000)
     })
 })
