14e1bbcc20592f1b070e52f245391d189534c45f
Tabassum Kabir <tabassumkabir123@gmail.com>
2023-08-11 21:19:28 +0600

    update

--- cypress/e2e/Modules/Admin.cy.js
+++ cypress/e2e/Modules/Admin.cy.js
@@ -1,3 +1,5 @@
+import 'cypress-file-upload';
+
 require('cypress-xpath')
 
 describe('Admin',()=>{
@@ -71,7 +73,7 @@ describe('Admin',()=>{
         cy.get('.oxd-button--label-danger').click()
     })
 
-    it.only('Jobs',()=>{
+    it('Jobs:pay grade',()=>{
 
         cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
 
@@ -88,4 +90,78 @@ describe('Admin',()=>{
         cy.get(':nth-child(3) > .oxd-table-row > :nth-child(1) > .oxd-table-card-cell-checkbox > .oxd-checkbox-wrapper > label').click()
 
     })
+
+    it('Jobs:edit pay grade',()=>{
+
+        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+
+        //click on admin 
+        cy.get(':nth-child(1) > .oxd-main-menu-item').click()
+
+        //click on Jobs
+        cy.get(':nth-child(2) > .oxd-topbar-body-nav-tab-item').click()
+        cy.wait(3000)
+        //select pay grade
+        cy.contains("Pay Grades").click()
+        //click on edit on grade 3
+        cy.get("div[class='orangehrm-container'] div:nth-child(3) div:nth-child(1) div:nth-child(4) div:nth-child(1) button:nth-child(2) i:nth-child(1)").click()
+        //clear the input box
+        cy.get("div[class='oxd-input-group oxd-input-field-bottom-space'] div input[class='oxd-input oxd-input--active']").clear()
+        //type in the input box
+        cy.get(':nth-child(2) > .oxd-input').type("Grade three")
+        //save it 
+        cy.get("button[type='submit']").click()
+
+
+    })
+
+    it('Jobs:delete pay grade',()=>{
+
+        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+
+        //click on admin 
+        cy.get(':nth-child(1) > .oxd-main-menu-item').click()
+
+        //click on Jobs
+        cy.get(':nth-child(2) > .oxd-topbar-body-nav-tab-item').click()
+        cy.wait(3000)
+        //select pay grade
+        cy.contains("Pay Grades").click()
+        //delete a pay grade
+        cy.get("div:nth-child(5) div:nth-child(1) div:nth-child(4) div:nth-child(1) button:nth-child(1) i:nth-child(1)").click()
+        //click on diaglouge box
+        cy.get('.oxd-button--label-danger').click()
+
+})
+
+     it.only('Corporate branding',()=>{
+
+     cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+
+     //click on admin 
+     cy.get(':nth-child(1) > .oxd-main-menu-item').click()
+     //click on more
+     cy.get('.oxd-topbar-body-nav > ul > :nth-child(5)').click()
+     //click on corporate branding
+     cy.contains("Corporate Branding").click()
+     //primary font color 
+     cy.get(':nth-child(3) > .oxd-input-group > .orangehrm-color-input-wrapper > .oxd-color-input > .oxd-color-input-preview').click()
+     //choose a color
+     cy.get('.oxd-color-picker-palette').click()
+     //put the hex code 
+     cy.get('.oxd-color-picker > .oxd-input').clear().type("#800000").type('{enter}')
+     //upload a pic
+     cy.xpath("(//div[@class='oxd-file-button'])[1]").attachFile("Random.png")
+     cy.wait(5000)
+     //click on publish 
+     //cy.get("button[type='submit']").click()
+     cy.wait(3000)
+
+
+     })
+
+
+
+
+
 })
